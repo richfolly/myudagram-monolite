6ec79e0399a8f796b4a3458ae8baaf61fd189367
richfolly <follodeh65@gmail.com>
2022-05-31 16:51:54 +0000

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=newproject
+export POSTGRES_PASSWORD=newproject
+export POSTGRES_HOST=newproject.cknvg3ve0oha.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=newproject
+export AWS_BUCKET=arn:aws:s3:::dms2021
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
